40f915a9e837fd37225d5a32574557e55ed7d9e4
Sergi Jacas <sergi.jacas@tmtfactory.com>
2020-06-09 16:49:51 +0200

    Clear api

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -11,15 +11,6 @@ export class ApiService {
 
   constructor() { }
 
-  /*
-    Crearía un servicio para simular el recibo de los datos de la api rest para poder
-    usarlos en las diferentes vistas que tuviera, si fuera necesario.
-    
-    El archivo de los datos es .ts porque con .jsaon me está dando un error que ahora 
-    mismo no encuentro solución
-      ERROR in undefined is not iterable (cannot read property Symbol(Symbol.iterator))
-  */
-
   getSource(): sources {
     return source;
   }
