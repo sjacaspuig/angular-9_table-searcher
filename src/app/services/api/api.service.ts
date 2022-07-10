76964ecad06b39b768689afecc747f76155a729c
sjacas <sergi.jacas@wezaam.com>
2022-07-10 13:25:58 +0200

    Refactor type and constant names

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { source, sources, country } from 'src/app/assets/datasource';
-import { population, populations } from 'src/app/assets/info-population';
-import moment from 'moment';
+import { Sources } from 'src/app/models/sources';
+import { Populations } from 'src/app/models/populations';
+import { SOURCE } from 'src/app/constants/sources';
+import { POPULATION } from 'src/app/constants/population';
 
 @Injectable({
   providedIn: 'root'
@@ -11,21 +12,11 @@ export class ApiService {
 
   constructor() { }
 
-  getSource(): sources {
-    return source;
+  getSource(): Sources {
+    return SOURCE;
   }
 
-  getPopulation(): populations {
-
-    population.population.person.forEach(item => {
-      item.datebirthday = moment(item.datebirthday).format('YYYY-MM-DD');
-      item.lastModification = moment(item.lastModification).format('YYYY-MM-DD HH:mm');
-      const prefix: country[] = source.data.country.filter((c: country) => c.id === item['country-id']);
-      item.phone = '+' + prefix[0].prefix + ' ' + item.phone;
-    });
-
-    console.log(population);
-
-    return population;
+  getPopulation(): Populations {
+    return POPULATION;
   }
 }
